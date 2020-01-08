18dbc1ae4ae1bdb7ccc8816fd9f097352cd1ed09
Casper <joeharris461767@gmail.com>
2020-01-08 23:36:51 +0000

    Add unit case tests

---
+++ src/index.test.js
@@ -0,0 +1,29 @@
+const Quickcord = require('./index');
+const dotenv = require('dotenv').config();
+
+console.log('Testing Initiated...');
+
+const quickcord = new Quickcord.Client(process.env.BOT_TOKEN, '.');
+
+console.log("Test 1 -> started");
+
+quickcord.on('loaded', message => {
+    if(message !== "Connection to Discord API established") {
+        throw new Error("Loaded event is being emitted but does not return a valid response");
+    }
+});
+
+console.log("Test 1 -> passed");
+
+console.log("Test 2 -> started");
+
+const embed = new Quickcord.Embed({
+    title: 'test',
+    description: 'testing'
+});
+
+if(embed === undefined) {
+    throw new Error("Embed could not be created");
+}
+
+console.log("Test 2 -> passed")
